e6db02ea3341f07ee55243e24307c1f483839c51
WON <won.develop@gmail.com>
2023-02-27 23:38:40 +0900

    [leetcode-84]<stack>

---
+++ leetcode/84/84.ts
@@ -0,0 +1,15 @@
+type StackElement = [number, number];
+function largestRectangleArea(heights: number[]): number {
+  const stack: StackElement[] = [];
+  let res = 0;
+  heights.concat(0).forEach((height, idx) => {
+    let startIndex = idx;
+    while (stack.length && stack[stack.length - 1][1] > height) {
+      let [pos, height] = stack.pop() as StackElement;
+      res = Math.max(res, (idx - pos) * height);
+      startIndex = pos;
+    }
+    stack.push([startIndex, height]);
+  })
+  return res;
+};
